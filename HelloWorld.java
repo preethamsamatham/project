58d6653b6f0452f6ca85f4caa7a5de20a0dba175
Preetham <preethamsamatham@gmail.com>
2019-03-18 15:44:04 +0530

    HelloWorld file added

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HelloWorld!");
+		Emp1 e=new Emp1("IIHT",10,2000);
+		System.out.println(e.getEmp1Details());
+		Emp1 e1=new Emp1("legato",12,30000);
+		System.out.println(e1.getEmp1Details());
+		Emp1 e2=new Emp1("Mohan",24);
+		System.out.println(e2.getEmp1Details());
+		Emp1 e3=new Emp1(23,100000);
+		System.out.println(e3.getEmp1Details());
+		
+		/*e.name="preetham";
+		e.age=23;
+		e.salary=100000;
+		System.out.println(e.getEmp1Details());
+		e.name="venkatesh";
+		e.age=30;
+		e.salary=200000;
+		System.out.println(e.getEmp1Details());*/
+		
+		
+		
+
+	}
+
+}
